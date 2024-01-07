86be8ec5c921a74c1ee23568b31c6509248ab54d
Angelo Verlain <geoangercola@gmail.com>
2024-01-08 01:05:17 +0200

    don't log env

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,8 +1,6 @@
 import { type PageProps } from "$fresh/server.ts";
 import { Head } from "../components/Head.tsx";
 
-console.log("Deno", Deno.env.toObject());
-
 export default function App({ Component }: PageProps) {
   return (
     <html>
