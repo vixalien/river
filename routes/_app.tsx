c3d78f64a1bb69f29dd91827c3396fd5541acae9
Angelo Verlain <geoangercola@gmail.com>
2024-01-08 00:58:47 +0200

    copy head

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,12 +1,13 @@
 import { type PageProps } from "$fresh/server.ts";
+import { Head } from "../components/Head.tsx";
+
+console.log("Deno", Deno.env.toObject());
+
 export default function App({ Component }: PageProps) {
   return (
     <html>
       <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>app</title>
-        <link rel="stylesheet" href="/styles.css" />
+        <Head />
       </head>
       <body>
         <Component />
