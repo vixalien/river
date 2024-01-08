53b6ac43a3dac958223ce71b163ff18e3ddacf4d
Angelo Verlain <geoangercola@gmail.com>
2024-01-09 01:32:43 +0200

    show download and share icons

--- components/Photo.tsx
+++ components/Photo.tsx
@@ -1,5 +1,6 @@
 import cn from "https://esm.sh/classnames@2.5.1";
 import ColorHash from "color-hash";
+import { basename } from "$std/path/basename.ts";
 
 import { ImageMedium } from "vsco-api";
 
@@ -20,9 +21,13 @@ interface PaginationButtonProps {
 }
 
 export function Photo({ matches, image, previous, next }: ImageProps) {
+  const env = Deno.env.toObject();
+
   // check slug
   const slug = image._id;
   const id = `id-${slug}`;
+  const imageUrl = imageLink(image, 480);
+  const capture_date = new Date(image.capture_date_ms);
 
   const tint = colorHash.hex(slug);
 
@@ -50,7 +55,7 @@ export function Photo({ matches, image, previous, next }: ImageProps) {
     >
       <img
         loading="lazy"
-        src={imageLink(image, 480)}
+        src={imageUrl}
         width={image.width}
         height={image.height}
       />
@@ -75,6 +80,66 @@ export function Photo({ matches, image, previous, next }: ImageProps) {
         title="Go to next photo"
         label="Next"
       />
+      <ul className="links top photodetail-links">
+        {/* <li className="caption">
+          <span className="caption-text">{image.description}</span>
+        </li>
+        <li className="date">
+          <code>
+            <time dateTime={capture_date.toISOString()}>
+              {capture_date.toLocaleDateString("en-IE")}
+            </time>
+          </code>
+        </li> */}
+        {env.ALLOW_IMAGE_SHARING == "1" && (
+          <li class="share">
+            <a
+              onclick={`shareImage('{{ image.basename }}','{{ slug | relative_url }}');`}
+              title="Share this photo"
+            >
+              Share
+            </a>
+          </li>
+        )}
+        {env.ALLOW_ORIGINAL_DOWNLOAD == "1" && (
+          <li class="download">
+            <a
+              href={imageUrl}
+              download={imageName(image)}
+              class=""
+              title="Download this image"
+            >
+              Download
+            </a>
+          </li>
+        )}
+      </ul>
+
+      <ul className="meta">
+        {env.ALLOW_IMAGE_SHARING == "1" && (
+          <li class="share">
+            <a
+              onclick={`shareImage('{{ image.basename }}','{{ slug | relative_url }}');`}
+              className="gridview-button share"
+              title="Share this photo"
+            >
+              Share
+            </a>
+          </li>
+        )}
+        {env.ALLOW_ORIGINAL_DOWNLOAD == "1" && (
+          <li class="download">
+            <a
+              href={imageUrl}
+              download={imageName(image)}
+              className="gridview-button download"
+              title="Download this image"
+            >
+              Download
+            </a>
+          </li>
+        )}
+      </ul>
     </li>
   );
 }
@@ -84,4 +149,8 @@ function imageLink(image: ImageMedium["image"], width: number) {
   url.searchParams.set("w", width.toString());
 
   return url.toString();
+}
+
+function imageName(image: ImageMedium["image"]) {
+  return basename(image.responsive_url);
 }
