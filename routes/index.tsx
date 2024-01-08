eb52f13a397340e3f84c8bc7f0724fd6cd56273c
Angelo Verlain <geoangercola@gmail.com>
2024-01-09 01:59:42 +0200

    add homepage links

--- routes/index.tsx
+++ routes/index.tsx
@@ -16,6 +16,69 @@ export default function Grid({ url, data }: PageProps<GridData>) {
   return (
     <main>
       <Photos images={media} url={url} />
+      <ul class="links bottom">
+        {env.ALLOW_ORDER_SORT_CHANGE == "1" && (
+          <li class="sort">
+            <a rel="me" href="#" title="Reverse sort order">Sort</a>
+          </li>
+        )}
+        {env.SHOW_RSS_FEED == "1" && (
+          <li class="rss">
+            <a
+              rel="alternate"
+              type="application/rss+xml"
+              href="/feed.xml"
+              title="RSS Feed"
+            >
+              RSS Feed
+            </a>
+          </li>
+        )}
+        {env.TWITTER_USERNAME != "" && (
+          <li class="twitter">
+            <a
+              rel="me"
+              href={`https://twitter.com/${env.TWITTER_USERNAME}`}
+              title={`@${env.TWITTER_USERNAME} on Twitter`}
+            >
+              Twitter
+            </a>
+          </li>
+        )}
+        {env.GITHUB_USERNAME != "" && (
+          <li class="github">
+            <a
+              rel="me"
+              href={`https://github.com/${env.GITHUB_USERNAME}`}
+              title={`@${env.GITHUB_USERNAME} on Github`}
+            >
+              Github
+            </a>
+          </li>
+        )}
+        {env.INSTAGRAM_USERNAME != "" && (
+          <li class="instagram">
+            <a
+              rel="me"
+              href={`https://instagram.com/${env.INSTAGRAM_USERNAME}`}
+              title={`@${env.INSTAGRAM_USERNAME} on Instagram`}
+            >
+              Github
+            </a>
+          </li>
+        )}
+        {env.CUSTOM_LINK_URL != "" && env.CUSTOM_LINK_NAME != "" && (
+          <li class="link">
+            <a
+              rel="me"
+              href={env.CUSTOM_LINK_URL}
+              title={env.CUSTOM_LINK_NAME}
+            >
+              {env.CUSTOM_LINK_NAME}
+            </a>
+          </li>
+        )}
+      </ul>
     </main>
   );
 }
